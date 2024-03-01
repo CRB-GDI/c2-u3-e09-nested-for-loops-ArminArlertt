4b7ed159386d0c1307476f652fd36eb1d019379d
ArminArlertt <145461564+ArminArlertt@users.noreply.github.com>
2024-03-01 09:29:07 -0500

    sorta confused

---
+++ exercise 9/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace exercise_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int cols = 1; cols > 10; cols++);
+            {
+                //print header 
+                Console.Write("{0, 5}" ,cols) ;
+            }
+            // print the table rows 
+        }
+    }
+}
