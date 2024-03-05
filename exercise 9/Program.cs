1c9e81eb7eab8025301417b19fe3d48e77d03a55
ArminArlertt <145461564+ArminArlertt@users.noreply.github.com>
2024-03-05 10:03:41 -0500

    finished the first part

--- exercise 9/Program.cs
+++ exercise 9/Program.cs
@@ -1,15 +1,33 @@
-﻿namespace exercise_9
+﻿using System.Security.Cryptography;
+
+namespace exercise_9
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            for (int cols = 1; cols > 10; cols++)
+            
             {
-                //print header 
-                Console.Write("{0, 5}" ,cols);
+                //PROBLEM 1
+                //Prompt for row width 
+                
+                Console.Write("number of width:");
+                int rowWidth = int.Parse (Console.ReadLine());
+                Console.Write("enter number of rows: ");
+                int numRows = int.Parse (Console.ReadLine());
+
+                //print rows 
+                Console.WriteLine();
+                for (int i = 1 ; i <= numRows; i++)
+                {
+                    for (int j = 1 ; j <= rowWidth; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
             }
-            // print the table rows 
+           
         }
     }
 }
